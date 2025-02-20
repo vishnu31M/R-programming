0faad6a23555479b4a5a30844e7b3706be076ab6
vishnu31M <vishnuundersquad@gmail.com>
2025-02-20 15:38:37 +0530

    Add files via upload

---
+++ 18)Write a R program to create an array with three columns, three rows, and two tables taking two vectors as input to the array. Print the array..R
@@ -0,0 +1,7 @@
+vector1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)  
+vector2 <- c(10, 11, 12, 13, 14, 15, 16, 17, 18) 
+combined_values <- c(vector1, vector2)
+dimensions <- c(3, 3, 2)
+my_array <- array(combined_values, dim = dimensions)
+cat("3x3x2 Array:\n")
+print(my_array)
